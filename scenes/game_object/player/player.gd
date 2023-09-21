8bd8d12ac05f06251bf90c4fd403674e743e2e94
Furkan Baytak <furkanbaytak@hotmail.com>
2023-09-21 16:21:29 +0300

    21.09.2023
    -added health
    -added end screen
    -added difficulty
    -improving ui

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,8 +3,18 @@ extends CharacterBody2D
 const MAX_SPEED = 150
 const ACCELERATION_SMOOTHING = 25
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
+
+var number_colliding_bodies = 0
+
 func _ready():
-	pass 
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited) 
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 func _process(delta):
@@ -21,3 +31,31 @@ func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	health_component.damage(1)
+	damage_interval_timer.start()
+	
+	
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
